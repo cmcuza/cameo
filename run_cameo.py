1c952fe6a4d59fbb8495e82bd5ada844549a8a72
Carlos Muniz Cuza <38991005+cmcuza@users.noreply.github.com>
2025-02-25 11:00:14 +0100

    Pass y copy to simplify_by_blocking

--- run_cameo.py
+++ run_cameo.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     y = np.squeeze(data_loader.data.values)
     x = np.arange(y.shape[0])
     hops = np.log(y.shape[0])*10
-    cameo_out = simplify_by_blocking(x, y, hops, acf, acf_threshold)
+    cameo_out = simplify_by_blocking(x, y.copy(), hops, acf, acf_threshold)
     decompressed_points = np.interp(np.arange(y.shape[0]), x[cameo_out], y[cameo_out])
     print('Compression ratio:', round(y.shape[0]/np.sum(cameo_out), 2))
     print('Decompression MSE:', np.mean((decompressed_points-y)**2))
